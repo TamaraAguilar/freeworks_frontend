e854a4b4dc0e314386db26d115ea81d538f2f6bd
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-09-08 20:23:21 -0300

    feat: añadir vista principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard';
+import { ProjectList } from './components/project-list/project-list';
+import { DeliverableFormComponent } from './components/deliverable-form/deliverable-form';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'projects', component: ProjectList },
+  { path: 'deliverables/new', component: DeliverableFormComponent }
+];
